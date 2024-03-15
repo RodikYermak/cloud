a04933c89a025f5b963e49ad279599ff8a318126
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-03-14 17:34:15 -0700

    initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/entities/user.entity';
+import { FileEntity } from './files/entities/file.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'bubble.db.elephantsql.com',
+      port: 5432,
+      username: 'crpyxuct',
+      password: 'OhRwTueBEpbtZ_KNBN5Pz5Lhu74G-Yx7',
+      database: 'crpyxuct',
+      entities: [UserEntity, FileEntity],
+      synchronize: true,
+    }),
+    UsersModule,
+    FilesModule,
+  ],
+  exports: [],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { FileEntity } from '../../files/entities/file.entity';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  fullName: string;
+
+  @OneToMany(() => FileEntity, (file) => file.user)
+  files: FileEntity[];
+}
